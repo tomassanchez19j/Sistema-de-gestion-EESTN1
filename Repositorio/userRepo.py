4f9a8c428572ddfe54125316849a65d4ca6647c6
sosaaa123 <lautasosita097@gmail.com>
2025-09-10 12:33:24 -0300

    comm

--- Repositorio/userRepo.py
+++ Repositorio/userRepo.py
@@ -1,5 +1,8 @@
 from conexion.conexion import Conexion
 
+
+#El unico atributo que van a tener los repositorios es un objeto conexion
+#Se arma todo en el main
 class UserRepo:
     def __init__(self, conexion: Conexion):
         self.conexion = conexion
